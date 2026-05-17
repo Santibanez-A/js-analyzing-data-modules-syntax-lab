e74f94bd189a7bf0a0f02eef1d2d6d61a58955e2
Adrian Santibanez <santibanez1996@gmail.com>
2026-05-16 22:25:58 -0500

    Complete modules and syntax lab

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  args.forEach(userArray => {
+    combinedObject.users = [
+      ...combinedObject.users,
+      ...userArray
+    ];
+  });
+
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 
 module.exports = {
